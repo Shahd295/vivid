73f1d12a945a64d7f0cf35de88d569019f94295c
Shahd <shahdmohamedmcr@gmail.com>
2024-08-26 01:28:12 +0300

    "Remove deploy workflow and update vite.config.js"

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vivid-store/"
+  base: "/vivid/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        404: '404.html',
+      },
+    },
+  },
 })
