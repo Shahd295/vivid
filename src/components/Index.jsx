6d244c9ec774559e462c470c8fcfd8a7f4ace895
Shahd Mohamed <shahdmohamedmcr@gmail.com>
2024-12-15 15:53:35 +0200

    Update Index.jsx

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -7,7 +7,7 @@ const ShopSection = () => {
     {
       title: "Custom Woodwork",
       subtitle: "Up to 25% off",
-      imageUrl: "https://plus.unsplash.com/premium_photo-1661778812498-d56d7f09d1fd?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      imageUrl: "https://plus.unsplash.com/premium_photo-1661778812498-d56d7f09d1fd?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D%22",
       cols: "col-span-12 md:col-span-6",
     },
     {
